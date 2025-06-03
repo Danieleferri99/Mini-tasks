cca95d5e1809ece95e291b05973ad655406bdf29
Danieleferri99 <ferridaniele.1999@gmail.com>
2025-06-04 00:10:11 +0200

    Update lista nomi con while true.py

--- lista nomi con while true.py
+++ lista nomi con while true.py
@@ -15,7 +15,7 @@ lunghezza = len(nome)
 print(f"i tuoi nomi sono: {counter} ")
 for nome in nomi:
     print(f"- {nome}")
-    if len(nome) > 10: 
+    if lunghezza > 10: 
         print(f"il tuo nome '{nome}' è troppo lungo è di {lunghezza} caratteri, arrivederci ")
     
         
